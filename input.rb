fb9e0d96bb7b3c13e3ca8f9d0502ca35c68d327c
trikyas <mooneychad@yahoo.com>
2017-04-12 20:24:13 +1000

    making things awesome

---
+++ input.rb
@@ -0,0 +1,50 @@
+# text = "Today, I wrote code"
+# text = "Tomorrow I will code, too"
+# puts text
+
+
+#••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+# puts "Enter your name:"
+# name = gets
+# chomped_name = name.chomp
+#
+# greeting = "Hello, #{chomped_name}.  You are awesome!"
+# puts greeting
+
+#••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+        #   The Above snippet will give the same result as below
+#••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+# puts "Enter your name:"
+# name = gets.chomp
+#
+# greeting = "Hello, #{name}.  You are awesome!"
+# puts greeting
+
+#••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+ #  Ask user for their name and ask them to enter an item they own.
+#••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+# puts "Enter your name:"
+# name = gets.chomp
+#
+# puts "Enter something you own:"
+# own = gets.chomp
+#
+#
+# greeting = "Hello, #{name}.  What an excellent #{own} you have!"
+# puts greeting
+
+#••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+ #  A program that insults the person who uses it.
+#••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••
+
+puts "Who are you?:"
+name = gets.chomp
+
+puts "What is your Hobby?"
+own = gets.chomp
+
+
+greeting = "Well, #{name}.  #{own} is pretty crap. Buy a dog instead!"
+puts greeting
